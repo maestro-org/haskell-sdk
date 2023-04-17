85f355eeb509b2c6db16c4ded259e351905f65f3
Piyush thapa <piyush.codelabs@gmail.com>
2023-04-17 21:00:14 +0545

    Integrate Asset & Address APi to root API

--- maestro-api/src/Maestro/API.hs
+++ maestro-api/src/Maestro/API.hs
@@ -2,12 +2,16 @@ module Maestro.API where
 
 import           Data.Proxy           (Proxy (..))
 import           Maestro.API.Accounts
+import           Maestro.API.Address
+import           Maestro.API.Assets
 import           Servant.API
 import           Servant.API.Generic
 
 data MaestroApi route  = Routes
   {
     _accounts :: route :- "accounts" :> ToServantApi AccountsAPI
+  , _address  :: route :- "addresses" :> ToServantApi AddressAPI
+  , _assets   :: route :- "assets" :> ToServantApi AssetsAPI
   } deriving(Generic)
 
 api :: Proxy (ToServantApi MaestroApi)
