ea0dba4eb02bf8176c8af282685fab8f4f711807
sourabhxyz <swasti@sourabh.xyz>
2023-09-21 15:25:13 +0530

    Drop V0 support - code improvements

--- test/Maestro/Test/Backoff.hs
+++ test/Maestro/Test/Backoff.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Maestro.Test.Backoff where
 
-import Test.Hspec
+import           Test.Hspec
 
-import Control.Concurrent (MVar, ThreadId, newMVar, takeMVar, putMVar, newEmptyMVar, forkFinally)
-import Control.Exception (try)
-import Control.Monad (void)
-import Data.Text (pack)
-import Maestro.Client.V1
-import Maestro.Types.V1
-import System.Environment (getEnv)
+import           Control.Concurrent (MVar, ThreadId, forkFinally, newEmptyMVar,
+                                     newMVar, putMVar, takeMVar)
+import           Control.Exception  (try)
+import           Control.Monad      (replicateM_)
+import           Data.Text          (pack)
+import           Maestro.Client.V1
+import           Maestro.Types.V1
+import           System.Environment (getEnv)
 
 
 spec_backoff :: Spec
@@ -26,7 +29,7 @@ doConcCall backoffSettings = do
     maestroKey <- pack <$> getEnv  "MAESTRO_API_KEY"
     env <- mkMaestroEnv @'V1 maestroKey Preprod backoffSettings
     children <- newMVar []
-    void $ mapM (forkChild children) $ replicate 30 $ task env
+    replicateM_ 30 (forkChild children (task env))
     waitForChildren children
   where
     task :: MaestroEnv 'V1 -> IO Ret
@@ -40,25 +43,25 @@ doConcCall backoffSettings = do
             (Just False)
         ) ["addr_test1vqj247zdmh7n9g46ukk59k2yxeslevzhah0uj3t0t450x3ggycpxj"]
 
-forkChild :: MVar [MVar Ret] -> IO (Ret) -> IO ThreadId
+forkChild :: MVar [MVar Ret] -> IO Ret -> IO ThreadId
 forkChild children action = do
     mvar :: MVar Ret <- newEmptyMVar
     childs <- takeMVar children
     putMVar children (mvar:childs)
-    forkFinally action $ \ret -> case ret of
-      Left _ -> putMVar mvar $ Left $ MaestroError "client finished abruptly"
+    forkFinally action $ \case
+      Left _     -> putMVar mvar $ Left $ MaestroError "client finished abruptly"
       Right ret' -> putMVar mvar ret'
 
 waitForChildren :: MVar [MVar Ret] -> IO ()
 waitForChildren children = do
   cs <- takeMVar children
   case cs of
-    []   -> return ()
-    m:ms -> do
+    []     -> return ()
+    m : ms -> do
       putMVar children ms
       ret <- takeMVar m
       case ret of
         Left _ -> error "failed"
-        _ -> waitForChildren children
+        _      -> waitForChildren children
 
 
