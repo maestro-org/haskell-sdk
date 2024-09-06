23f609f474a3e006c1b0d3fa3261d50a88203c19
sourabhxyz <swasti@sourabh.xyz>
2024-09-06 15:29:26 +0530

    feat: remove unnecessary `Maybe` wrap over some of the protocol params

--- src/Maestro/Types/V1/General.hs
+++ src/Maestro/Types/V1/General.hs
@@ -310,15 +310,15 @@ data ProtocolParameters = ProtocolParameters
   {
     protocolParametersCollateralPercentage            :: !Natural
   -- ^ The percentage of the transactions fee which must be provided as collateral when including non-native scripts.
-  , protocolParametersConstitutionalCommitteeMaxTermLength :: !(Maybe Natural)
-  , protocolParametersConstitutionalCommitteeMinSize :: !(Maybe Natural)
-  , protocolParametersDelegateRepresentativeDeposit :: !(Maybe AsAda)
-  , protocolParametersDelegateRepresentativeMaxIdleTime :: !(Maybe Natural)
-  , protocolParametersDelegateRepresentativeVotingThresholds :: !(Maybe DRepVotingThresholds)
+  , protocolParametersConstitutionalCommitteeMaxTermLength :: Natural
+  , protocolParametersConstitutionalCommitteeMinSize :: Natural
+  , protocolParametersDelegateRepresentativeDeposit :: AsAda
+  , protocolParametersDelegateRepresentativeMaxIdleTime :: Natural
+  , protocolParametersDelegateRepresentativeVotingThresholds :: DRepVotingThresholds
   , protocolParametersDesiredNumberOfStakePools       :: !Natural
   -- The equilibrium target number of stake pools. This is the \"k\" incentives parameter from the design document, /AKA/ @n_opt@, @stake_pool_target@.
-  , protocolParametersGovernanceActionDeposit :: !(Maybe AsAda)
-  , protocolParametersGovernanceActionLifetime :: !(Maybe Natural)
+  , protocolParametersGovernanceActionDeposit :: AsAda
+  , protocolParametersGovernanceActionLifetime :: Natural
   , protocolParametersMaxBlockBodySize                :: !AsBytes
   -- ^ Maximum block body size.
   , protocolParametersMaxBlockHeaderSize              :: !AsBytes
@@ -329,7 +329,7 @@ data ProtocolParameters = ProtocolParameters
   -- ^ The maximum number of execution memory & steps allowed to be used in a single block.
   , protocolParametersMaxExecutionUnitsPerTransaction :: !(MemoryCpuWith Natural)
   -- ^ The maximum number of execution memory & steps allowed to be used in a single transaction.
-  , protocolParametersMaxReferenceScriptsSize :: !(Maybe AsBytes)
+  , protocolParametersMaxReferenceScriptsSize :: AsBytes
   , protocolParametersMaxTransactionSize              :: !AsBytes
   -- ^ Maximum transaction size.
   , protocolParametersMaxValueSize                    :: !AsBytes
@@ -338,7 +338,7 @@ data ProtocolParameters = ProtocolParameters
   -- ^ The constant factor for the minimum fee calculation /AKA/ @min_fee_a@ and @tx_fee_per_byte@.
   , protocolParametersMinFeeConstant                  :: !AsAda
   -- ^ The linear factor for the minimum fee calculation for given epoch /AKA/ @min_fee_b@ and @tx_fee_fixed@.
-  , protocolParametersMinFeeReferenceScripts :: !(Maybe MinFeeReferenceScripts)
+  , protocolParametersMinFeeReferenceScripts :: MinFeeReferenceScripts
   , protocolParametersMinStakePoolCost                :: !AsAda
   -- ^ The minimum value that stake pools are permitted to declare for their cost parameter.
   , protocolParametersMinUtxoDepositCoefficient       :: !Natural
@@ -357,7 +357,7 @@ data ProtocolParameters = ProtocolParameters
   -- The influence of the pledge in stake pool rewards. This is the \"a_0\" incentives parameter from the design document.
   , protocolParametersStakePoolRetirementEpochBound   :: !EpochNo
   -- ^ The maximum number of epochs into the future that stake pools are permitted to schedule a retirement /AKA/ @pool_retire_max_epoch@, @e_max@.
-  , protocolParametersStakePoolVotingThresholds :: !(Maybe StakePoolVotingThresholds)
+  , protocolParametersStakePoolVotingThresholds :: !StakePoolVotingThresholds
   , protocolParametersTreasuryExpansion               :: !MaestroRational
   -- ^ The fraction of the fee pot each epoch that goes to the treasury. This is the \"tau\" incentives parameter from the design document, /AKA/ @treasury_cut@.
   , protocolParametersVersion                         :: !ProtocolVersion
